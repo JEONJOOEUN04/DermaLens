df76467dee42b83dcc50d8d3b6c2da072a03513f
jooeun04 <jitchive24@gmail.com>
2026-06-04 21:19:35 +0900

    🐛 fix: media 파일 항상 서빙 (OCR 서버 이미지 다운로드용)

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 
 def health_check(request):
@@ -25,8 +25,6 @@ urlpatterns = [
 
     path("api/admin/", include("admin_stats.urls")),
 
+    # media 파일 서빙 (DEBUG 무관 — OCR 서버가 업로드 이미지 다운로드해야 함)
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
